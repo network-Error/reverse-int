88ec948c968814687a24673cb3b5d8538e5661fe
network-Error <iamimew@gmail.com>
2022-07-02 22:42:51 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+
+    let num = n + '';
+
+    if (num < 0) {
+        let minusNum = Math.abs(num);
+        let toStringNum = String(minusNum);
+        let toNumMinus = toStringNum.split('').reverse().join('');
+
+        return Number(toNumMinus);
+    } else {
+        let reverseEl = num.split('').reverse().join('');
+        let toNum = Number(reverseEl);
+
+        return toNum;
+    }
 }
